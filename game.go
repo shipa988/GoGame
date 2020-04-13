d11f2d8c0b5f92d326ede3e24e4d40eead45e5dd
Марина <Marine@MBP-Marina.Dlink>
2020-04-13 22:10:40 +0300

    try 5

--- game.go
+++ game.go
@@ -1,17 +1,18 @@
 package game
 
 import (
-	"bytes"
-	"github.com/hajimehoshi/ebiten/examples/resources/images"
+
 	"log"
+	"math/rand"
+	"os"
+	"time"
 
 	//"bytes"
 	"image"
-	_ "image/png"
-	//"log"
+	"image/png"
+	e "github.com/hajimehoshi/ebiten"
+	"github.com/markbates/pkger"
 
-	"github.com/hajimehoshi/ebiten"
-	//"github.com/hajimehoshi/ebiten/examples/resources/images"
 )
 
 const (
@@ -25,25 +26,185 @@ const (
 	frameNum    = 8
 )
 
-var (
-	count       = 0
-	runnerImage *ebiten.Image
-)
 
-func Update(screen *ebiten.Image) error {
-	count++
 
-	if ebiten.IsDrawingSkipped() {
+var frames map[string]Frames
+var frame int
+var unit *Unit
+
+func Update(screen *e.Image) error {
+	frame++
+
+	if e.IsDrawingSkipped() {
 		return nil
 	}
 
-	op := &ebiten.DrawImageOptions{}
+	op := &e.DrawImageOptions{}
 	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
 	op.GeoM.Translate(screenWidth/2, screenHeight/2)
-	i := (count / 5) % frameNum
-	sx, sy := frameOX+i*frameWidth, frameOY
-	screen.DrawImage(runnerImage.SubImage(image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image), op)
+
+	sprites = append(sprites, Sprite{
+		Frames: frames[unit.Skin+"_"+unit.Action].Frames,
+		Frame:  int(unit.Frame),
+		X:      unit.X,
+		Y:      unit.Y,
+		//Side:   unit.Side,
+		Config: frames[unit.Skin+"_"+unit.Action].Config,
+	})
+	for _,sprite:=range sprites  {
+		img, err := e.NewImageFromImage(sprite.Frames[(frame/7+sprite.Frame)%4], e.FilterDefault)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		err= screen.DrawImage(img, op)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	}
 	return nil
+}
+type Sprite struct {
+	Frames []image.Image
+	Frame  int
+	X      float64
+	Y      float64
+	//Side   game.Direction
+	Config image.Config
+}
+
+type Unit struct {
+	Id int
+	X float64
+	Y float64
+	Frame int32
+	Skin string
+	Action string
+	Speed int
+
+
+}
+type Frames struct {
+	Frames []image.Image
+	image.Config
+}
+var sprites []Sprite
+func LoadResources() (map[string]Frames, error) {
+	images := map[string]image.Image{}
+	cfgs := map[string]image.Config{}
+	sprites := map[string]Frames{}
+
+	pkger.Walk("./resources/sprites", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		f,err:=pkger.Open(info.Name())
+		if err != nil {
+			return err
+		}
+		img, err := png.Decode(f)
+		if err != nil {
+			return  err
+		}
+		cfg, err := png.DecodeConfig(f)
+
+		images[info.Name()] = img
+		cfgs[info.Name()] = cfg
+		return nil
+	})
+
+
+	sprites["big_demon_idle"] = Frames{
+		Frames: []image.Image{
+			images["big_demon_idle_anim_f0.png"],
+			images["big_demon_idle_anim_f1.png"],
+			images["big_demon_idle_anim_f2.png"],
+			images["big_demon_idle_anim_f3.png"],
+		},
+		Config: cfgs["big_demon_idle_anim_f0.png"],
+	}
+	sprites["big_demon_run"] = Frames{
+		Frames: []image.Image{
+			images["big_demon_run_anim_f0.png"],
+			images["big_demon_run_anim_f1.png"],
+			images["big_demon_run_anim_f2.png"],
+			images["big_demon_run_anim_f3.png"],
+		},
+		Config: cfgs["big_demon_run_anim_f0.png"],
+	}
+
+	sprites["big_zombie_idle"] = Frames{
+		Frames: []image.Image{
+			images["big_zombie_idle_anim_f0.png"],
+			images["big_zombie_idle_anim_f1.png"],
+			images["big_zombie_idle_anim_f2.png"],
+			images["big_zombie_idle_anim_f3.png"],
+		},
+		Config: cfgs["big_zombie_idle_anim_f0.png"],
+	}
+	sprites["big_zombie_run"] = Frames{
+		Frames: []image.Image{
+			images["big_zombie_run_anim_f0.png"],
+			images["big_zombie_run_anim_f1.png"],
+			images["big_zombie_run_anim_f2.png"],
+			images["big_zombie_run_anim_f3.png"],
+		},
+		Config: cfgs["big_zombie_run_anim_f0.png"],
+	}
+
+	sprites["elf_f_idle"] = Frames{
+		Frames: []image.Image{
+			images["elf_f_idle_anim_f0.png"],
+			images["elf_f_idle_anim_f1.png"],
+			images["elf_f_idle_anim_f2.png"],
+			images["elf_f_idle_anim_f3.png"],
+		},
+		Config: cfgs["elf_f_idle_anim_f0.png"],
+	}
+	sprites["elf_f_run"] = Frames{
+		Frames: []image.Image{
+			images["elf_f_run_anim_f0.png"],
+			images["elf_f_run_anim_f1.png"],
+			images["elf_f_run_anim_f2.png"],
+			images["elf_f_run_anim_f3.png"],
+		},
+		Config: cfgs["elf_f_run_anim_f0.png"],
+	}
+	sprites["floor_1"] = Frames{
+		Frames: []image.Image{images["floor_1.png"]},
+		Config: cfgs["floor_1.png"],
+	}
+	sprites["floor_2"] = Frames{
+		Frames: []image.Image{images["floor_2.png"]},
+		Config: cfgs["floor_2.png"],
+	}
+	sprites["floor_3"] = Frames{
+		Frames: []image.Image{images["floor_3.png"]},
+		Config: cfgs["floor_3.png"],
+	}
+	sprites["floor_4"] = Frames{
+		Frames: []image.Image{images["floor_4.png"]},
+		Config: cfgs["floor_4.png"],
+	}
+	sprites["floor_5"] = Frames{
+		Frames: []image.Image{images["floor_5.png"]},
+		Config: cfgs["floor_5.png"],
+	}
+	sprites["floor_6"] = Frames{
+		Frames: []image.Image{images["floor_6.png"]},
+		Config: cfgs["floor_6.png"],
+	}
+	sprites["floor_7"] = Frames{
+		Frames: []image.Image{images["floor_7.png"]},
+		Config: cfgs["floor_7.png"],
+	}
+	sprites["floor_8"] = Frames{
+		Frames: []image.Image{images["floor_8.png"]},
+		Config: cfgs["floor_8.png"],
+	}
+
+	return sprites, nil
 }
 
 func init() {
@@ -56,9 +217,21 @@ func init() {
 	//    This works even on browsers.
 	// 3) Use ebitenutil.NewImageFromFile to create an ebiten.Image directly from a file.
 	//    This also works on browsers.
-	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	skins := []string{"big_demon", "big_zombie", "elf_f"}
+	unit = &Unit{
+		Id:     1,
+		X:      rnd.Float64()*300 + 10,
+		Y:      rnd.Float64()*220 + 10,
+		Frame:  int32(rnd.Intn(4)),
+		Skin:   skins[rnd.Intn(len(skins))],
+		Action: "idle",
+		Speed:  1,
+	}
+	var err error
+	frames, err =LoadResources()
 	if err != nil {
 		log.Fatal(err)
 	}
-	runnerImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/shipa988/ebitentest
 
-go 1.13
+go 1.14
 
-require github.com/hajimehoshi/ebiten v1.10.5
+require (
+	github.com/hajimehoshi/ebiten v1.10.5
+	github.com/markbates/pkger v0.15.1
+)
